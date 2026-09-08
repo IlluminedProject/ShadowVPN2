49a4f076c063198fca7bdd7b6ba0e6e340447e0b
SKProCH <skproch@yandex.ru>
2026-09-08 04:09:05 +0300

    Remove ability to view settings from default users

--- ShadowVPN2/Infrastructure/Authentication/AppRoles.cs
+++ ShadowVPN2/Infrastructure/Authentication/AppRoles.cs
@@ -1,7 +1,6 @@
 namespace ShadowVPN2.Infrastructure.Authentication;
 
-public static class AppRoles
-{
+public static class AppRoles {
     public const string Administrator = "Administrator";
     public const string User = "User";
 
@@ -9,14 +8,11 @@ public static class AppRoles
     /// Defines the default permissions for each role.
     /// </summary>
     public static readonly IReadOnlyDictionary<string, IReadOnlyList<string>> DefaultRolePermissions =
-        new Dictionary<string, IReadOnlyList<string>>
-        {
+        new Dictionary<string, IReadOnlyList<string>> {
             [Administrator] = AppPermissions.All,
 
-            [User] =
-            [
-                AppPermissions.Nodes.View,
-                AppPermissions.Settings.View
+            [User] = [
+                AppPermissions.Nodes.View
             ]
         };
 }
