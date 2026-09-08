e423cbe95858ad9da622ddab8a81f5f7a29a3d4e
SKProCH <skproch@yandex.ru>
2026-09-08 03:41:30 +0300

    Fixes compilator nullability warnings

--- ShadowVPN2/Infrastructure/Authentication/AdvancedUserStore.cs
+++ ShadowVPN2/Infrastructure/Authentication/AdvancedUserStore.cs
@@ -4,28 +4,25 @@ using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
 using Raven.Identity;
 using IdentityRole = Raven.Identity.IdentityRole;
-using IdentityUser = Raven.Identity.IdentityUser;
 
 namespace ShadowVPN2.Infrastructure.Authentication;
 
+#pragma warning disable CS8613, CS8767
 public class AdvancedUserStore<TUser, TRole> : UserStore<TUser, TRole>, IUserPasskeyStore<TUser>
     where TUser : AdvancedIdentityUser where TRole : IdentityRole, new()
+#pragma warning restore CS8613, CS8767
 {
     public AdvancedUserStore(Func<IAsyncDocumentSession> getSession, ILogger<AdvancedUserStore<TUser, TRole>> logger,
-        IOptions<RavenDbIdentityOptions> options) : base(getSession, logger, options)
-    {
+        IOptions<RavenDbIdentityOptions> options) : base(getSession, logger, options) {
     }
 
     public AdvancedUserStore(IAsyncDocumentSession session, ILogger<AdvancedUserStore<TUser, TRole>> logger,
-        IOptions<RavenDbIdentityOptions> options) : base(session, logger, options)
-    {
+        IOptions<RavenDbIdentityOptions> options) : base(session, logger, options) {
     }
 
-    public Task AddOrUpdatePasskeyAsync(TUser user, UserPasskeyInfo passkey, CancellationToken cancellationToken)
-    {
+    public Task AddOrUpdatePasskeyAsync(TUser user, UserPasskeyInfo passkey, CancellationToken cancellationToken) {
         var existing = user.Passkeys.FirstOrDefault(p => p.CredentialId.SequenceEqual(passkey.CredentialId));
-        if (existing != null)
-        {
+        if (existing != null) {
             user.Passkeys.Remove(existing);
         }
 
@@ -33,24 +30,21 @@ public class AdvancedUserStore<TUser, TRole> : UserStore<TUser, TRole>, IUserPas
         return Task.CompletedTask;
     }
 
-    public Task<IList<UserPasskeyInfo>> GetPasskeysAsync(TUser user, CancellationToken cancellationToken)
-    {
+    public Task<IList<UserPasskeyInfo>> GetPasskeysAsync(TUser user, CancellationToken cancellationToken) {
         return Task.FromResult<IList<UserPasskeyInfo>>(user.Passkeys);
     }
 
-    public async Task<TUser?> FindByPasskeyIdAsync(byte[] credentialId, CancellationToken cancellationToken)
-    {
+    public async Task<TUser?> FindByPasskeyIdAsync(byte[] credentialId, CancellationToken cancellationToken) {
         return await DbSession.Query<TUser>()
             .FirstOrDefaultAsync(u => u.Passkeys.Any(p => p.CredentialId == credentialId), cancellationToken);
     }
 
-    public Task<UserPasskeyInfo?> FindPasskeyAsync(TUser user, byte[] credentialId, CancellationToken cancellationToken)
-    {
+    public Task<UserPasskeyInfo?>
+        FindPasskeyAsync(TUser user, byte[] credentialId, CancellationToken cancellationToken) {
         return Task.FromResult(user.Passkeys.FirstOrDefault(p => p.CredentialId.SequenceEqual(credentialId)));
     }
 
-    public Task RemovePasskeyAsync(TUser user, byte[] credentialId, CancellationToken cancellationToken)
-    {
+    public Task RemovePasskeyAsync(TUser user, byte[] credentialId, CancellationToken cancellationToken) {
         user.Passkeys.RemoveAll(p => p.CredentialId.SequenceEqual(credentialId));
         return Task.CompletedTask;
     }
