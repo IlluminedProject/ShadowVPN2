bed025f4b62c8447169ed29f4d71b1d620af417a
SKProCH <skproch@yandex.ru>
2026-09-24 14:53:06 +0300

    Allow multiple protocols

--- ShadowVPN2/Data/SingBox/Contributors/WireGuardAmneziaConfigContributor.cs
+++ ShadowVPN2/Data/SingBox/Contributors/WireGuardAmneziaConfigContributor.cs
@@ -7,57 +7,56 @@ namespace ShadowVPN2.Data.SingBox.Contributors;
 public sealed class WireGuardAmneziaConfigContributor(WireGuardKeyService keyService) : ISingBoxConfigContributor {
     public async Task ContributeAsync(SingBoxConfig config, IReadOnlyList<ProtocolGlobalSettings> protocols,
         IReadOnlyList<EntityClient> clients) {
-        var settings = protocols.OfType<AwgGlobalSettings>().FirstOrDefault(s => s.Enabled);
-        if (settings is null) return;
+        foreach (var settings in protocols.OfType<AwgGlobalSettings>().Where(s => s.Enabled)) {
+            EndpointConfig endpoint;
+            if (settings.IsAmneziaWg) {
+                endpoint = new AwgEndpointConfig {
+                    Tag = $"wireguard-clients-{settings.ListenPort}",
+                    Mtu = settings.Mtu,
+                    Address = [settings.ServerAddress],
+                    PrivateKey = settings.PrivateKey,
+                    ListenPort = settings.ListenPort,
+                    Jc = settings.Jc,
+                    Jmin = settings.Jmin,
+                    Jmax = settings.Jmax,
+                    S1 = settings.S1,
+                    S2 = settings.S2,
+                    S3 = settings.S3,
+                    S4 = settings.S4,
+                    H1 = settings.H1,
+                    H2 = settings.H2,
+                    H3 = settings.H3,
+                    H4 = settings.H4,
+                    I1 = settings.I1,
+                    I2 = settings.I2,
+                    I3 = settings.I3,
+                    I4 = settings.I4,
+                    I5 = settings.I5
+                };
+            }
+            else {
+                endpoint = new WireGuardEndpointConfig {
+                    Tag = $"wireguard-clients-{settings.ListenPort}",
+                    Mtu = settings.Mtu,
+                    Address = [settings.ServerAddress],
+                    PrivateKey = settings.PrivateKey,
+                    ListenPort = settings.ListenPort
+                };
+            }
 
-        EndpointConfig endpoint;
-        if (settings.IsAmneziaWg) {
-            endpoint = new AwgEndpointConfig {
-                Tag = "wireguard-clients",
-                Mtu = settings.Mtu,
-                Address = [settings.ServerAddress],
-                PrivateKey = settings.PrivateKey,
-                ListenPort = settings.ListenPort,
-                Jc = settings.Jc,
-                Jmin = settings.Jmin,
-                Jmax = settings.Jmax,
-                S1 = settings.S1,
-                S2 = settings.S2,
-                S3 = settings.S3,
-                S4 = settings.S4,
-                H1 = settings.H1,
-                H2 = settings.H2,
-                H3 = settings.H3,
-                H4 = settings.H4,
-                I1 = settings.I1,
-                I2 = settings.I2,
-                I3 = settings.I3,
-                I4 = settings.I4,
-                I5 = settings.I5
-            };
+            var peers = endpoint is AwgEndpointConfig awgEndpoint
+                ? awgEndpoint.Peers
+                : ((WireGuardEndpointConfig)endpoint).Peers;
+
+            foreach (var client in clients.Where(c => c.IsEnabled)) {
+                await keyService.EnsureKeyAsync(client);
+                peers.Add(new WireGuardPeer {
+                    PublicKey = client.WireGuard!.PublicKey!,
+                    AllowedIps = [$"{client.GetAssignedIp()}/32"]
+                });
+            }
+
+            config.Endpoints.Add(endpoint);
         }
-        else {
-            endpoint = new WireGuardEndpointConfig {
-                Tag = "wireguard-clients",
-                Mtu = settings.Mtu,
-                Address = [settings.ServerAddress],
-                PrivateKey = settings.PrivateKey,
-                ListenPort = settings.ListenPort
-            };
-        }
-
-        var peers = endpoint is AwgEndpointConfig awgEndpoint
-            ? awgEndpoint.Peers
-            : ((WireGuardEndpointConfig)endpoint).Peers;
-
-        foreach (var client in clients.Where(c => c.IsEnabled)) {
-            await keyService.EnsureKeyAsync(client);
-            peers.Add(new WireGuardPeer {
-                PublicKey = client.WireGuard!.PublicKey!,
-                AllowedIps = [$"{client.GetAssignedIp()}/32"]
-            });
-        }
-
-        config.Endpoints.Add(endpoint);
     }
 }
